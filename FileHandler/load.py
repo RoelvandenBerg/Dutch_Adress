52dea1bafd17d6862701410acd60fc5c232ebda1
RoelvandenBerg <Roelvdberg@gmail.com>
2015-04-19 13:19:32 +0200

    Update load.py

--- FileHandler/load.py
+++ FileHandler/load.py
@@ -6,30 +6,55 @@ Created on 9 jan. 2015
 import re
 from statistics import median
 
+
+def re_split(sep, string):
+    '''
+    Splits a string on a seperator e.g. a newline symbol or a semicolon using
+    regular expressions.
+    '''
+    re_pattern = re.compile(r'''((?:[^''' + sep + '''"']|"[^"]*"|'[^']*')+)''')
+    return re_pattern.split(string)[1::2]
+
+
 def open_read(filename, encoding=None, sep=None):
-    re_pattern_end = re.compile(r'''((?:[^\n"']|"[^"]*"|'[^']*')+)''')
-    re_pattern_sep = re.compile(r'''((?:[^''' + sep + '''"']|"[^"]*"|'[^']*')+)''')
-    with open(filename, "r", encoding=encoding) as f:
-        c = f.read()
-        if sep:
-            content = [re_pattern_sep.split(x)[1::2] for x in re_pattern_end.split(c)[1::2]]
-        else:
-            content = re_pattern_end.split(c)[1::2]
+    '''
+    Reads filecontents from <filename> and splits on newline symbols and <sep>.
+    '''
+    with open(filename, "r", encoding=encoding) as f: #open file
+        c = f.read() #read file contents
+        if sep: #when sep exists: split file contents and on newline
+            content = [re_split(sep, x) for x in re_split('\n', c)]
+        else: 
+            content = re_split('\n', c)
     return content
 
 
+def try_unicode(filename, encoding, sep):
+    '''
+    Tries out different types of encodings given in the encoding parameter
+    '''
+    if len(encoding) == 0:
+        raise Exception("Decoding failed with given encodings")
+    try:
+        return open_read(filename, encoding[0], sep)
+    except UnicodeDecodeError:
+        return try_unicode(filename, encoding[1:], sep)
+
+
 def load(filename, sep=";", header=False, same_length=False, verbose=True):
+    '''
+    File loader, reads contents of a file and returns a list of its contents,
+    split on newline and/or seperator(sep).
+    Verbose toggles printing out statements to screen. 
+    Same_length checks wether the rowsize is constant and removes 
+    those rows that differ from the median row length.
+    '''
     if verbose:
         print('Loading %s...' % filename)
     
     #handling encoding
-    try:
-        content = open_read(filename, sep=sep)
-    except UnicodeDecodeError:
-        content = open_read(filename, 'utf8', sep)
-    except UnicodeDecodeError:
-        content = open_read(filename, 'latin1', sep)
-    
+    try_unicode(filename, ['utf8', 'latin1'], sep)
+
     #settings
     if header:
         #remove header row
@@ -45,13 +70,10 @@ def load(filename, sep=";", header=False, same_length=False, verbose=True):
 
 
 def csv(filename, sep=";", header=False, verbose=True):
+    'wrapper function that loads a csv, splits on newline and seperator'
     return load(filename, sep, header, verbose=verbose)
 
 
 def txt(filename):
+    'wrapper function that loads a csv (only splits on newline)'
     return load(filename, sep=None)
-
-
-def dirlist():
-    return {x: y for x, y in csv("dirlist.cfg", verbose=False)}
-    
